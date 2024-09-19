02fef7ea9bb279c0414f4c3a585cc1a1e6c99190
coding4freedom <chawkins2010@hotmail.com>
2024-09-19 02:45:16 -0500

    auth controllers added, middleware setups

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+//routes to get current user
+router.get("/me", protectRoute, getMe);
+
+// routes for all post methods
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
 export default router;

e618d2245f787a0517e4a03218899760cbadc191
coding4freedom <chawkins2010@hotmail.com>
2024-09-21 15:02:47 -0500

    Updated route naming convention, Notification route and controller added feature working

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 //imported routes
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 // imported utilities
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -29,6 +30,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
 
 
 app.listen(PORT, () => {
